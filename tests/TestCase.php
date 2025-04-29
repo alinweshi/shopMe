a54905b1b1b7a64fe8501859bf9450a96a6ff264
alinweshi <alinweshi@gmail.com>
2025-04-29 04:14:11 +0300

    update test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Mock Vite for tests
+        \Illuminate\Support\Facades\Vite::shouldReceive('asset')
+            ->andReturn('http://localhost:3000/resources/css/app.css');
+    }
 }
